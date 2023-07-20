dee4416c64130ff3b5a97e016be0b84437a16cd8
panoskouff <me@panoskouf.xyz>
2023-07-20 11:30:33 +0200

    add navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { NavMenu } from '@/components/NavMenu/NavMenu';
 import './globals.css';
 import type { Metadata } from 'next';
 
@@ -13,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <NavMenu />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/NavMenu/NavMenu.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import s from './NavMenu.module.css';
+
+export const NavMenu = () => (
+  <nav className={s.nav}>
+    <Link href='/'>
+      <Image src='/logo.svg' alt='Logo' width={216} height={30} />
+    </Link>
+    <ul>
+      <li>
+        <Link href='/about'>About</Link>
+      </li>
+      <li>
+        <Link href='/blog'>Blog</Link>
+      </li>
+      <li>
+        <Link href='/users'>Users</Link>
+      </li>
+    </ul>
+  </nav>
+);
