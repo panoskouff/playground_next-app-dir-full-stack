09820a66654116ee141db0f77c27c80cc3b61131
panoskouff <me@panoskouf.xyz>
2023-07-20 16:34:38 +0200

    install and configure auth.js and support login with github

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,15 @@
+import NextAuth from 'next-auth/next';
+import type { NextAuthOptions } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+
+export const OPTIONS: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+};
+
+const handler = NextAuth(OPTIONS);
+export { handler as GET, handler as POST };
