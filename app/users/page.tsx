ab68523babf5043b17d3cb17cde90d86c998079c
panoskouff <me@panoskouf.xyz>
2023-07-21 13:53:30 +0200

    add profile page for users

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,12 +1,12 @@
 import UserCard from '@/components/UserCard/UserCard';
-import styles from './page.module.css';
+import s from './page.module.css';
 import { prisma } from '@/lib/prisma';
 
 export default async function Users() {
   const users = await prisma.user.findMany();
 
   return (
-    <div className={styles.grid}>
+    <div className={s.grid}>
       {users.map((user) => {
         return <UserCard key={user.id} {...user} />;
       })}
